4e49357d4eb91d7b54a17c4f4a715948b9770fd5
Willy Blandin <wblandin@gmail.com>
2020-10-17 17:00:22 +0200

    v0.3: notify when battery is low

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(non_upper_case_globals)]
+
 extern crate cocoa;
 extern crate core_foundation;
 extern crate core_foundation_sys;
@@ -21,6 +23,9 @@ use objc::declare::ClassDecl;
 use objc::runtime::{Object, Sel, YES};
 use std::ffi::CStr;
 use std::os::raw::c_char;
+use std::process::{Command, Output};
+use std::result::Result;
+use std::sync::Mutex;
 use std::thread;
 use std::time;
 
@@ -36,38 +41,37 @@ extern "C" {
     pub fn IOPSGetTimeRemainingEstimate() -> CFTimeInterval;
 }
 
-#[allow(non_upper_case_globals)]
 pub const kIOPSACPowerValue: &str = "AC Power";
-#[allow(non_upper_case_globals)]
 pub const kIOPSBatteryPowerValue: &str = "Battery Power";
-#[allow(non_upper_case_globals)]
 pub const kIOPSIsChargingKey: &str = "Is Charging";
-#[allow(non_upper_case_globals)]
 pub const kIOPSMaxCapacityKey: &str = "Max Capacity";
-#[allow(non_upper_case_globals)]
 pub const kIOPSCurrentCapacityKey: &str = "Current Capacity";
-#[allow(non_upper_case_globals)]
 pub const kIOPSPowerSourceStateKey: &str = "Power Source State";
-#[allow(non_upper_case_globals)]
 pub const kIOPSTimeToEmptyKey: &str = "Time to Empty";
-#[allow(non_upper_case_globals)]
 pub const kIOPSTimeToFullChargeKey: &str = "Time to Full Charge";
-#[allow(non_upper_case_globals)]
 pub const kIOPSTimeRemainingUnknown: CFTimeInterval = -1.0;
-#[allow(non_upper_case_globals)]
 pub const kIOPSTimeRemainingUnlimited: CFTimeInterval = -2.0;
 
 static mut STATUS_ITEM: Option<id> = None;
+const NOTIF_THRESHOLD: f64 = 5.;
+
+unsafe fn status_item() -> id {
+    STATUS_ITEM.unwrap()
+}
 
 pub struct FromId {
-    id: id
+    id: id,
+}
+
+unsafe fn id_to_string(id: id) -> String {
+    let chars: *const c_char = msg_send![id, UTF8String];
+    let cstr = CStr::from_ptr(chars);
+    cstr.to_owned().into_string().unwrap()
 }
 
 impl From<FromId> for String {
     fn from(x: FromId) -> Self {
-        let chars: *const c_char = unsafe { msg_send![x.id, UTF8String] };
-        let cstr = unsafe { CStr::from_ptr(chars) };
-        cstr.to_owned().into_string().unwrap()
+        unsafe { id_to_string(x.id) }
     }
 }
 
@@ -85,7 +89,7 @@ impl From<FromId> for bool {
 
 fn nsdict_get<T: From<FromId>>(dict: id, key: &str) -> T {
     let k = unsafe { NSString::alloc(nil).init_str(key) };
-    let v: FromId = unsafe { msg_send![dict, objectForKey:k] };
+    let v: FromId = unsafe { msg_send![dict, objectForKey: k] };
     From::from(v)
 }
 
@@ -93,7 +97,7 @@ fn human_time(minutes: i64) -> String {
     return format!("{}:{:02}", minutes / 60, minutes % 60);
 }
 
-fn generate_title() -> String {
+fn compute_title_and_percent() -> (String, f64) {
     let blob = unsafe { IOPSCopyPowerSourcesInfo() };
     let nsary: id = unsafe { IOPSCopyPowerSourcesList(blob) } as id;
     // TODO(willy) smarter power source selection using kIOPSInternalBatteryType
@@ -105,7 +109,7 @@ fn generate_title() -> String {
     let current_pct = 100. * current_cap as f64 / max_cap as f64;
 
     #[allow(non_upper_case_globals)]
-    match state.as_ref() {
+    let title = match state.as_ref() {
         kIOPSBatteryPowerValue => {
             let mins = nsdict_get::<i32>(nsdict, kIOPSTimeToEmptyKey);
             format!(
@@ -130,10 +134,17 @@ fn generate_title() -> String {
                 current_pct
             )
         }
-        &_ => {
-            String::from("\u{1F914}")
-        }
-    }
+        &_ => String::from("\u{1F914}"),
+    };
+
+    (title, current_pct)
+}
+
+fn send_notification(msg: &str, say: &str, voice: &str) -> Result<Output, std::io::Error> {
+    let notif_cmd = format!("display notification \"{}\" with title \"batterybar\"", msg);
+    let say_cmd = format!("say \"{}\" -v \"{}\" -r 160", say, voice);
+    Command::new("osascript").args(&["-e", &notif_cmd]).output()?;
+    Command::new("bash").args(&["-c", &say_cmd]).output()
 }
 
 fn main() {
@@ -159,13 +170,27 @@ fn main() {
         extern "C" fn application_did_finish_launching(_: &Object, _: Sel, _: id) {
             info!("application did finish launching");
 
-            thread::spawn(|| loop {
-                let title = generate_title();
+            let sent_notif_mutex = Mutex::new(false);
+
+            thread::spawn(move || loop {
+                let (title, pct) = compute_title_and_percent();
+
                 unsafe {
-                    STATUS_ITEM.unwrap().setTitle_(
-                        NSString::alloc(nil).init_str(&title).autorelease()
-                    );
+                    status_item().setTitle_(NSString::alloc(nil).init_str(&title).autorelease());
                 }
+
+                let mut sent_notif = sent_notif_mutex.lock().unwrap();
+                if pct <= NOTIF_THRESHOLD && !*sent_notif {
+                    let notif_msg = format!("Battery at {}%", pct);
+                    let say_msg = format!("배터리가 {}% 입니다", pct);
+                    let say_voice = "Yuna";
+                    send_notification(&notif_msg, &say_msg, &say_voice)
+                        .expect("could not display notification");
+                    *sent_notif = true;
+                } else if pct > NOTIF_THRESHOLD && *sent_notif {
+                    *sent_notif = false;
+                }
+
                 thread::sleep(time::Duration::from_millis(5000));
             });
         }
@@ -177,7 +202,7 @@ fn main() {
         let delegate_class = decl.register();
         let delegate_object: id = msg_send![delegate_class, new];
 
-        msg_send![app, setDelegate: delegate_object];
+        let _: () = msg_send![app, setDelegate: delegate_object];
 
         app.run();
     }
